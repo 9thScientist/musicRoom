2e23df5c2b44ee670217466d940a541b0a4c7243
escolhido <celocaridade@gmail.com>
2016-01-26 20:23:14 +0000

    Disconnect

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+var http= require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
 var urlencodedParser = bodyParser.urlencoded({extended: true});
+app.use(urlencodedParser);
+
+app.use(express.static(__dirname));
+app.set('view engine', 'ejs');
 
 var onUsers = ["Joana", "Jorge"];
-
-app.use(urlencodedParser);
-app.use(express.static(__dirname));
-
-app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 	res.sendFile('views/pages/index.html', {root: __dirname});
@@ -18,10 +19,15 @@ app.get('/', function(req, res) {
 app.post('/login', function(req, res) {
 	onUsers.push(req.body.username);
 	res.render ('pages/main', {onUsers});
-
-	console.log (req.body.username + ' connected.');
 });
 
-app.listen(3000, function() {
+io.on('connection', function(socket) {
+	console.log('New user connected.');
+	socket.on('disconnect', function() {
+		console.log('User disconnected.');
+	});
+});
+
+http.listen(3000, function() {
 	console.log('Connected on port 3000.');
 });
