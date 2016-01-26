ead0b0d52663a3511cf0d62a150e95ac8a46995c
escolhido <celocaridade@gmail.com>
2016-01-26 23:35:38 +0000

    Continuação do desenvolvimento do disconnect

--- server.js
+++ server.js
@@ -11,23 +11,31 @@ app.use(express.static(__dirname));
 app.set('view engine', 'ejs');
 
 var onUsers = ["Joana", "Jorge"];
+var usersID = {}; 
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/views/pages/index.html');
-
+	res.sendFile('views/pages/index.html', {root: __dirname});
 });
 
 app.post('/login', function(req, res) {
-	onUsers.push(req.body.username);
-	res.render ('pages/main', {onUsers});
-});
+	var username = req.body.username;
 
-io.on('connection', function(socket) {
-	console.log('New user connected.');
-	socket.on('disconnect', function() {
-		console.log('User disconnected.');
+	onUsers.push(username);
+	res.render ('pages/main', {onUsers});
+
+	io.on('connection', function(socket) {
+		console.log(username + ' user connected.');
+		usersID[socket.id] = username;
+
+		socket.on('disconnect', function() {
+			var ind = onUsers.indexOf(usersID[socket.id]);
+			username = onUsers.splice(ind, 1);
+			console.log(username + ' disconnected.');
+			delete usersID[socket.id];
+		});
 	});
 });
+
 
 http.listen(3000, function() {
 	console.log('Connected on port 3000.');
