7b226d8a985f00537221d28a8f72472ace524976
escolhido <celocaridade@gmail.com>
2016-01-27 13:38:12 +0000

    não sei o que aconteceu

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var app = express();
 var http= require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var urlencodedParser = bodyParser.urlencoded({extended: true});
 
 app.use(urlencodedParser);
 app.use(cookieParser());
+app.use(express.session({secret: '140e9masO85saiu!'}))
 
 app.use(express.static(__dirname));
 app.set('view engine', 'ejs');
@@ -20,19 +21,13 @@ var userSID;
 
 
 app.get('/', function(req, res) {
-
-	console.log (req.cookies.session);
-
-	if (req.cookies.length != undefined) res.render
-	else res.sendFile('views/pages/index.html', {root: __dirname});
-
+	res.sendFile(__dirname + '/views/pages/index.html');
 });
 
 app.post('/login', function(req, res) {
 	username = req.body.username;
 	onUsers.push(username);
 
-	res.setHeader('Set-Cookie', ['session=' + userSID]);
 	res.render ('pages/main', {onUsers});
 
 });
@@ -41,12 +36,6 @@ io.on('connection', function(socket) {
 	console.log(username + ' user connected.');
 	userSID = socket.id;
 	usersID[socket.id] = username;
-
-/*
-	app.get('/cookie', function(req, res) {
-		res.cookie(cookie_name, userSID);
-	});
-*/
 
 	socket.on('disconnect', function() {
 		var ind = onUsers.indexOf(usersID[socket.id]);
