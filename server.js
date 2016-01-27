e0eb8791776c05318b77524201cf3d3d6312ee7f
escolhido <celocaridade@gmail.com>
2016-01-27 16:20:25 +0000

    Comportamento do connect/disconnect melhorada

--- server.js
+++ server.js
@@ -8,40 +8,71 @@ var session = require('express-session');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({secret: '140e9masO85saiu!',
 				 resave: false,
-				 saveUninitialized: false
-				}))
+				 saveUninitialized: false 
+				}));
+
 
 app.use(express.static(__dirname));
 app.set('view engine', 'ejs');
 
 var onUsers = ["Joana", "Jorge"];
 var usersID = {}; 
-var username;
-var userSID;
 
+var session;
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/views/pages/index.html');
+	session = req.session;
+
+	if (session.username)
+		res.render('pages/main', {onUsers});
+	else
+		res.sendFile(__dirname + '/views/pages/index.html');
 });
 
 app.post('/login', function(req, res) {
-	username = req.body.username;
-	onUsers.push(username);
+	session = req.session;
 
+	if (!session.username){
+		session.username = req.body.username;
+		onUsers.push(session.username);
+	}
+	
+	consolo.log(session.id);
 	res.render ('pages/main', {onUsers});
-
 });
 
+function isOnline(username) {
+	var repeat = false;
+
+	for (var sock in usersID){
+		if (usersID[sock] == username){
+			repeat = true;
+			break;
+		}
+	}
+
+	return repeat;
+}
+
 io.on('connection', function(socket) {
-	console.log(username + ' user connected.');
-	userSID = socket.id;
-	usersID[socket.id] = username;
+	var repeat = isOnline(session.username);
+	usersID[socket.id] = session.username;
+	
+	if (!repeat)
+		console.log(session.username + ' user connected.');
 
 	socket.on('disconnect', function() {
-		var ind = onUsers.indexOf(usersID[socket.id]);
-		username = onUsers.splice(ind, 1);
-		console.log(username + ' disconnected.');
+		var username = usersID[socket.id];
 		delete usersID[socket.id];
+		repeat = isOnline(username);
+
+		if (!repeat){
+			var ind = onUsers.indexOf(username);
+			
+			onUsers.splice(ind, 1);
+			console.log(username + ' disconnected.');
+		}
+
 	});
 });
 
